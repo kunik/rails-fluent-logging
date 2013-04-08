eda37f88529d3e451da317638ff90e5a20e2a158
Taras Kunch <taras.kunch@gmail.com>
2013-04-08 12:04:42 +0300

    Implement helper for severity pretty printing

--- lib/rails-fluent-logging/json_formatter.rb
+++ lib/rails-fluent-logging/json_formatter.rb
@@ -2,6 +2,15 @@ require 'json-colorizer'
 
 module RailsFluentLogging
   class JsonFormatter < JsonColorizer
+    SEVERITY_COLORS = {
+      'DEBUG' =>   { color: :yellow },
+      'INFO' =>    { color: :cyan },
+      'WARN' =>    { color: :light_magenta },
+      'ERROR' =>   { color: :red },
+      'FATAL' =>   { background: :red, color: :white },
+      'UNKNOWN' => { color: :default }
+    }
+
     def initialize(schema, datetime_format)
       super(schema)
       @datetime_format = datetime_format
@@ -13,6 +22,10 @@ module RailsFluentLogging
 
     def message(s)
       s.to_s
+    end
+
+    def severity(s)
+      colorize(s[0].upcase, SEVERITY_COLORS[s] || SEVERITY_COLORS['UNKNOWN'])
     end
 
     alias :call :format
